e425f22ee1b337693af2205450bd3ba5bbc4060b
SScods <aasishsharma99911@gmail.com>
2020-12-11 08:57:40 +0530

    My Project

---
+++ HOMEE.php
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+	<tile>dgddd</tile>
+</head>
+
+
+<form action="index.php" method="post">
+	<input type="text" name="t1">
+	<input type="password" name="p">
+	<input type="submit" value="Login">
+</form>
+</html>
